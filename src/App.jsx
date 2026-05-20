b482e8e308a93ada9211b9cd55c64b7a0283b513
Dursun Köktürk <dursunserefkokturk@gmail.com>
2026-05-20 14:11:37 +0300

    src klasoru icindeki app.jsx component icinde food-card div'i icinde food bilgileri api uzerinden alinarak yazdirildi, app.css dosyasinda css duzenlemesi yapildi

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 export default function App() {
+
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    fetch('https://dummyjson.com/recipes')
+      .then(response => response.json())
+      .then((data) => {
+        setRecipes(data.recipes);
+      })
+    // .then(console.log);
+  }, [])
 
   return (
     <>
       <div className="general-title">
         <h1>Yemekler</h1>
+      </div>
+      <div className="food-card">
+        {recipes.map((recipe) => (
+          <div key={recipe.id} className='product-informations'>
+            <img src={recipe.image} alt="" />
+            <h5>{recipe.cuisine}</h5>
+            <h4>{recipe.name}</h4>
+          </div>
+        ))}
       </div>
     </>
   )
