031cade1bb0bc9617a75ea79be5292ccf12f66a9
Dursun Köktürk <dursunserefkokturk@gmail.com>
2026-05-22 10:08:42 +0300

    src klasoru icindeki app.jsx component icinde container div'i icindeki food-card-and-order-card div'inde desktop gorunum icin app.css dosyasinda css duzenlemesi yapildi islem devam edecek

--- src/App.jsx
+++ src/App.jsx
@@ -22,26 +22,28 @@ export default function App() {
         <div className="general-title">
           <h1>Meals</h1>
         </div>
-        <div className="food-card">
-          {recipes.map((recipe) => (
-            <div key={recipe.id} className='product-informations'>
-              <div className="image-wrapper">
-                <img src={recipe.image} className='product-photo' alt="" />
-                <button>
-                  <img src={ShoppingCard} alt="" />
-                  Add to Cart
-                </button>
+        <div className="food-card-and-order-card">
+          <div className="food-card">
+            {recipes.map((recipe) => (
+              <div key={recipe.id} className='product-informations'>
+                <div className="image-wrapper">
+                  <img src={recipe.image} className='product-photo' alt="" />
+                  <button>
+                    <img src={ShoppingCard} alt="" />
+                    Add to Cart
+                  </button>
+                </div>
+                <h5>{recipe.cuisine}</h5>
+                <h4>{recipe.name}</h4>
               </div>
-              <h5>{recipe.cuisine}</h5>
-              <h4>{recipe.name}</h4>
+            ))}
+          </div>
+          <div className="order-card">
+            <h2>Your Cart (0)</h2>
+            <div className="items-card">
+              <img src={EmptyBasket} alt="" />
+              <h5>Your added items will appear here</h5>
             </div>
-          ))}
-        </div>
-        <div className="order-card">
-          <h2>Your Cart (0)</h2>
-          <div className="items-card">
-            <img src={EmptyBasket} alt="" />
-            <h5>Your added items will appear here</h5>
           </div>
         </div>
       </div>
